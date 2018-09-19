f5a719174d15ff4cc21d709588f838b098257f10
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2018-09-19 16:51:46 +0100

    Add to_upper filter for WTForms

--- dmutils/forms/filters.py
+++ dmutils/forms/filters.py
@@ -7,3 +7,9 @@ def strip_whitespace(value):
     if value is not None and hasattr(value, 'strip'):
         return value.strip()
     return value
+
+
+def to_upper(value):
+    if value is not None:
+        return value.upper()
+    return value
