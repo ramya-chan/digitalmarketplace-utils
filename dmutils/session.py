9e549c17cd49a78acd59811c8c065277c098b3a9
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-05-01 11:07:06 +0100

    Add support for local Redis setup

--- dmutils/session.py
+++ dmutils/session.py
@@ -4,10 +4,16 @@ import redis
 import dmutils.cloudfoundry as cf
 
 def init_app(app):
-    vcap_services = cf.get_vcap_services(app)
 
-    redis_service_name = app.config["DM_REDIS_SERVICE_NAME"]
-    redis_service = cf.get_service_by_name_from_vcap_services(vcap_services, redis_service_name)
+    if not app.config.get("SESSION_REDIS"):
+        if app.config["DM_ENVIRONMENT"] == "development":
+            app.config["SESSION_REDIS"] = redis.Redis()
+        else:
+            vcap_services = cf.get_vcap_services(app)
 
-    app.config["SESSION_REDIS"] = redis.from_url(redis_service["credentials"]["uri"])
+            redis_service_name = app.config["DM_REDIS_SERVICE_NAME"]
+            redis_service = cf.get_service_by_name_from_vcap_services(vcap_services, redis_service_name)
+
+            app.config["SESSION_REDIS"] = redis.from_url(redis_service["credentials"]["uri"])
+
     flask_session.Session(app)
